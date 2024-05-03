30083a60708bf04dfbe0f230f48e66365f5e8d0f
Younessboumlik <younessboumlik1949@gmail.com>
2024-05-03 04:09:13 +0100

    task 1

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Task 5"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Computes the sum of a list of floating-point numbers."""
+    return float(sum(input_list))
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Task 6'''
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Computes the sum of a list of integers and floating-point numbers'''
+    return float(sum(mxd_lst))
