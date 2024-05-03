c96088a2822221fbf539c95a9a17caee0efa68de
Younessboumlik <younessboumlik1949@gmail.com>
2024-05-03 03:34:01 +0100

    task 1

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Another task to use typing in python."""
+
+import math
+def floor(n: float) -> int:
+    """return the float of a number which is an integer."""
+    return math.floor(n)
