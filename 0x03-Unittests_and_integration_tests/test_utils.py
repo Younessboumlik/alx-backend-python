5bbe074cfce26ae3dc86aef3f7a97285d4ce0d6b
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-13 05:08:40 +0100

    fixing errors

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,11 +6,11 @@ from typing import Mapping, Sequence, Any
 
 class TestAccessNestedMap(unittest.TestCase):
   """ Tests the access_nested_map function """
-    @parameterized.expand([
+  @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
+  ])
   def test_access_nested_map(self, nested_map: Mapping,
                               path: Sequence, expected: int) -> None:
       """
