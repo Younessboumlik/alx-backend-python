632797b3c311ceff481df096a8d5609c793794db
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-13 05:21:18 +0100

    fixing indentation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
-""" this is a unittest for the function access_nested_map(nested_map: Mapping, path: Sequence) -> Any """
+""" This is a unittest for the function access_nested_map(nested_map: Mapping, path: Sequence) -> Any """
 import unittest
 from typing import Mapping, Sequence, Any
 from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
-  """ Tests the access_nested_map function """
-  @parameterized.expand([
+    """ Tests the access_nested_map function """
+    
+    @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
-  ])
-  def test_access_nested_map(self, nested_map: Mapping,
-                              path: Sequence, expected: int) -> None:
-      """
+    ])
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: Any) -> None:
+        """
         Test the access_nested_map method.
         Args:
-            nested_map (Dict): A dictionary that may have nested dictionaries
-            path (List, tuple, set): Keys to get to the required value in the
-                                     nested dictionary
-      """
-      answer = access_nested_map(nested_map, path)
-      self.assertEqual(answer, expected)
-  
+            nested_map (Mapping): A dictionary that may have nested dictionaries
+            path (Sequence): Keys to get to the required value in the nested dictionary
+            expected (Any): The expected value at the end of the path
+        """
+        answer = access_nested_map(nested_map, path)
+        self.assertEqual(answer, expected)
 
