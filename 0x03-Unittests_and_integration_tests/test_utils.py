45dab25b10c567c9e892438b3f097d0074debe5c
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-13 05:19:59 +0100

    importing libraries

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """ this is a unittest for the function access_nested_map(nested_map: Mapping, path: Sequence) -> Any """
 import unittest
 from typing import Mapping, Sequence, Any
+from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
